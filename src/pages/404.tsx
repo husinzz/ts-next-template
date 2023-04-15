e55b923661373aa3e2ff5b44a25388d4fba7348e
husinzz <its.husin@gmail.com>
2023-04-16 01:19:55 +0700

    feat : 404 page, index page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,20 +1,26 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import Link from "next/link";
 
 const NotFound: NextPage = () => {
   return (
-    <div className="flex flex-col h-full">
-      {/* Some header */}
-      <div className="flex flex-grow items-center justify-center">
-        <div className="text-center">
-          <p className="font-bold text-3xl">404</p>
-          <Link className="hover:text-blue-600" href={"/"}>
-            Go back
-          </Link>
+    <>
+      <Head>
+        <title>NOT FOUND</title>
+      </Head>
+      <div className="flex flex-col h-full">
+        {/* Some header */}
+        <div className="flex flex-grow items-center justify-center">
+          <div className="text-center">
+            <p className="font-bold text-3xl">404</p>
+            <Link className="hover:text-blue-600" href={"/"}>
+              Go back
+            </Link>
+          </div>
         </div>
+        {/* Some footer */}
       </div>
-      {/* Some footer */}
-    </div>
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div className="flex flex-col h-full">
-    {/* Some header */}
-    <div className="flex flex-grow items-center justify-center">
-      <div className="text-center">
-        <p className="font-bold text-3xl">The template works</p>
-        <p>Unless its not bold and huge</p>
+    <>
+      <Head>
+        <title>TS Next Template</title>
+      </Head>
+      <div className="flex flex-col h-full">
+        {/* Some header */}
+        <div className="flex flex-grow items-center justify-center">
+          <div className="text-center">
+            <p className="font-bold text-3xl">The template works</p>
+            <p>Unless its not bold and huge</p>
+          </div>
+        </div>
+        <p className="text-center">
+          This template is intended for fullstack apps
+        </p>
+        {/* Some footer */}
       </div>
-    </div>
-    <p className="text-center">This template is intended for fullstack apps</p>
-    {/* Some footer */}
-  </div>
-  )
+    </>
+  );
 }
